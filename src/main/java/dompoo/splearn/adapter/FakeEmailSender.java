2053818341daee1350b4588faa7673d03c42777a
Dompoo <dlckdrms0517@gmail.com>
2025-06-28 17:51:09 +0900

    feat: FakeEmailSender 작성

---
+++ src/main/java/dompoo/splearn/adapter/FakeEmailSender.java
@@ -0,0 +1,16 @@
+package dompoo.splearn.adapter;
+
+import dompoo.splearn.application.required.EmailSender;
+import dompoo.splearn.domain.Email;
+import org.springframework.context.annotation.Fallback;
+import org.springframework.stereotype.Component;
+
+@Fallback
+@Component
+public class FakeEmailSender implements EmailSender {
+
+  @Override
+  public void send(Email email, String subject, String body) {
+    System.out.println("이메일 전송 email : " + email.address() + ", subject : " + subject + ", body : " + body);
+  }
+}
