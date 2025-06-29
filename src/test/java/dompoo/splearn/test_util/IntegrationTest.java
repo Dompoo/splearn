bd0f8c82091ca15c2bb6544b1106197d820ca896
Dompoo <dlckdrms0517@gmail.com>
2025-06-30 00:56:14 +0900

    test: 2개였던 통합 테스트 관련 설정을 하나로 리팩터링

--- src/test/java/dompoo/splearn/test_util/IntegrationTest.java
+++ src/test/java/dompoo/splearn/test_util/IntegrationTest.java
@@ -1,19 +1,33 @@
 package dompoo.splearn.test_util;
 
+import dompoo.splearn.application.member.required.EmailSender;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
-@Import(SplearnTestConfig.class)
+@Import(IntegrationTest.DummyEmailSenderConfig.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public abstract class IntegrationTest {
 
   @PersistenceContext
   protected EntityManager em;
+
+  @TestConfiguration
+  public static class DummyEmailSenderConfig {
+
+    @Bean
+    public EmailSender emailSender() {
+      return (email, subject, body) -> {
+        // 아무것도 하지 않는다.
+      };
+    }
+  }
 }
